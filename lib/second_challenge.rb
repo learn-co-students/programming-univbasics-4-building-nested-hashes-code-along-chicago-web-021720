3a5b490a1f891f1ca7432f99fd65118a8dca265d
bryan lesht <bryan.lesht@colorado.edu>
2020-01-29 21:40:37 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,12 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
+    :montague => { :patriarch =>{},:matriarch =>{},:hero =>{},:hero_friends =>[]},
     :capulet => {
+      :patriarch =>{},:matriarch =>{},:heroine =>{}, :heroine_friends =>[]
+    }
+  
       #your key/value pairs here!
-      }}
+      }
 
   
 end
